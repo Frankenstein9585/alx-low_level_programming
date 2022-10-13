302ad9b11cc5220ca3f516ae2c16c8bf55ca7e37
Donald Peters <Donny9585@gmail.com>
2022-10-13 18:49:06 +0000

    Done task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  *main-Serves as the entry piont of a program
+ *   *initialize the strings
+ *    *write the strings out
+ *     *Return:if no error return zero, otherrwise return non zero value
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
