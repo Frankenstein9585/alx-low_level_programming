94ada4a916d1cd501613fbaf12a65f74cfa34852
Donald Peters <Donny9585@gmail.com>
2022-10-15 11:08:24 +0000

    Done task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Does more stuff
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
