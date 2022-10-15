ce1d6b7a3111fdd402b991799abc3268e3c11be4
Donald Peters <Donny9585@gmail.com>
2022-10-15 11:43:27 +0000

    Done task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Does stuff
+  *
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
