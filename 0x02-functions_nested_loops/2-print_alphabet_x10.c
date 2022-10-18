65ef902ff365cf9ed3b1ece312e3805d46dc7a65
Donald Peters <Donny9585@gmail.com>
2022-10-18 19:01:17 +0000

    DOne task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_alphabet - prints the alphabets 5 times
+  * 
+  * Return: Nothing
+  */
+void print_alphabet(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
