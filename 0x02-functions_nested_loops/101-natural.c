88c4507483a56345703e3c7006f6271d2d9a3a3e
Donald Peters <Donny9585@gmail.com>
2022-10-19 11:42:48 +0000

    Done task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - prints the sum of multiples less than 1024
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
