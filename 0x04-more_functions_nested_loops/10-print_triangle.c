8b6d5114790156c2d621582a7d55285d0412e721
ukcharlies <111245934+ukcharlies@users.noreply.github.com>
2022-10-21 00:36:18 +0100

    Modified

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,18 @@
 
 void print_triangle(int size)
 {
-		int i, j;
+                int i, j;
 
-		if (size <= 0)
-			_putchar('\n');
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('#');
-			_putchar('\n');
-		}
+                if (size <= 0)
+                        _putchar('\n');
+                for (i = 0; i < size; i++)
+                {
+                        for (j = 0; j < i; j++)
+                        {
+                                _putchar(' ');
+                        }
+                        _putchar('#');
+                        _putchar('\n');
+                }
 }
+
