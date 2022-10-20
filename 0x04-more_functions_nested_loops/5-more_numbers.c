db2fe3224d4dd59d75fd3ada77de945388cf5bcc
Donald Peters <Donny9585@gmail.com>
2022-10-20 21:10:13 +0000

    Done task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * more_numbers - prints 0 to 9 excluding 2 and 4
+  *
+  */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 15; ++j)
+		{
+			if (j >= 10)
+			{
+				putchar(j / 10 + 48);
+			}
+			putchar(j % 10 + 48);
+		}
+		putchar('\n');
+	}
+}
