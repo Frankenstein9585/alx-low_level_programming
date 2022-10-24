1d38f469fe02b5c28f471eba70d5562356b3b898
Donald Peters <Donny9585@gmail.com>
2022-10-24 17:04:39 +0000

    Done task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_rev - prints string in reverse order
+  *
+  * @s: string to be printed in reverse order
+  */
+void print_rev(char *s)
+{
+	int i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
