9c9cab00eb1fb6a80972c0f16a965378a992c85d
Donald Peters <Donny9585@gmail.com>
2022-10-24 22:00:33 +0000

    Done task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+/**
+  * puts_half - prints every other character of a string
+  *
+  * @str: string to be operated on
+  */
+void puts_half(char *str)
+{
+	int n = strlen(str) - 1;
+	int i;
+
+	if (n % 2 == 0)
+	{
+		i = n / 2 + 1
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = (n - 1) / 2 + 2;
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
