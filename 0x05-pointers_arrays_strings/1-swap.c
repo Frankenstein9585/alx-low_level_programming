639a5f6e9062c84f3b324d1d688bee717b9ddabb
Donald Peters <Donny9585@gmail.com>
2022-10-24 16:15:16 +0000

    Done task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * swap_int - swaps integers
+  *
+  * @a: operand
+  * @b: operand
+  */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y =  temp;
+}
