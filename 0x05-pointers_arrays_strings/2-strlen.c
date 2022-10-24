c51791fac94ad65775468b7391d64ac9d5157df6
Donald Peters <Donny9585@gmail.com>
2022-10-24 16:33:03 +0000

    Done task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strlen - returns the length of a string
+  * @s: string to be operated on
+  * Return: the length of string
+  */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
