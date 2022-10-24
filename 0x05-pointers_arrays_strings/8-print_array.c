c6e6af750abea6c2b517419fcc9d80fa03b64660
Donald Peters <Donny9585@gmail.com>
2022-10-24 22:30:49 +0000

    Done task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_array - prints every other character of a string
+  *
+  * @a: string to be operated on
+  * @n: number of elements to be printed
+  */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i]);
+	}
+	_putchar('\n');
+}
