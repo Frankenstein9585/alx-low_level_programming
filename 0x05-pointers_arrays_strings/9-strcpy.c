931dc53040ac60ca82a4b541c848b2df628067f2
Donald Peters <Donny9585@gmail.com>
2022-10-24 23:07:52 +0000

    Done task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - print array up to n elements
+ * @dest: array to be read
+ * @src: number to be read
+ * Return: always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
