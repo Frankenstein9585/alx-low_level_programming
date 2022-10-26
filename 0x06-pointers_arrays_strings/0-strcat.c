b013b8cd45a6ac01cc1653a87ca4c8e570014b97
Donald Peters <Donny9585@gmail.com>
2022-10-26 22:05:30 +0000

    Done task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,17 @@
   * @src: input string
   * Return: dest
   */
-char *_strcat(char *dest, char *src) 
+char *_strcat(char *dest, char *src)
 {
-    int a = strlen(dest);
-    int b = 0;
-    
-    while (src[b] != '\0') 
-    {
-        dest[a] = src[b];
-        a++;
-        b++;
-    }
-    return (dest);
+	int a = strlen(dest);
+	int b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
 }
 
