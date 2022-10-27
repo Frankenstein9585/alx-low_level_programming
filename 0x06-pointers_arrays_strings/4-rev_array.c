bef5c4be5cc002ec0ff84db2a9e5580da1d0cea6
Donald Peters <Donny9585@gmail.com>
2022-10-27 13:34:34 +0000

    Done task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * rev_array - compares two strings
+  *
+  * @a: array to be reversed
+  * @n: number of elements in the array
+  *
+  */
+void rev_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
