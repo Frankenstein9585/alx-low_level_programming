59f1c35c05a421fb9daf9d2153d6f0f7631778ef
Donald Peters <Donny9585@gmail.com>
2022-10-27 10:44:35 +0000

    Done task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int a = 0;
 
 	while (s1[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (0);
+		{
+			a = s1[i] - s2[i];
+			return (a);
+		}
+
 		i++;
 	}
-	if (s2[i] != '\0')
-		return (0);
-	return (1);
-
+	return (0);
 }
