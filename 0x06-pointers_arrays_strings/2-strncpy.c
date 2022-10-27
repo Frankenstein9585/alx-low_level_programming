8aac0c0b0454bb9a16c4131f28dcd3c828f1336d
Donald Peters <Donny9585@gmail.com>
2022-10-27 10:02:58 +0000

    Modified task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0;
 
-	while (a < n && src[i] != '\0')
+	while (a < n && src[a] != '\0')
 	{
 		dest[a] = src[a];
 		a++;
 	}
 
-	a = a;
 	while (a < n)
 	{
 		dest[a] = '\0';
@@ -24,4 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	return (dest);
 }
+int main()
+{
 
+}
