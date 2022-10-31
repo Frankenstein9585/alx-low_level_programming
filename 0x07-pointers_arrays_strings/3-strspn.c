59ed7733724a781c91c7c013747bd09960591c7c
Donald Peters <Donny9585@gmail.com>
2022-10-31 19:04:46 +0000

    Done task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _strspn - returns the number of bytes in the initial segement of s
+  * which consist only of bytes from accept
+  *
+  *
+  * @s: string
+  * @accept: another string
+  * Return: s
+  */
+char *_strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
