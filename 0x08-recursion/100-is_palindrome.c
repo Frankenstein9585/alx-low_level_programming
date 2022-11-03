366b7145463bd6f56844bd8e7cf6b8de2a1ede16
Donald Peters <Donny9585@gmail.com>
2022-11-03 09:40:03 +0000

    Modified task 7 again

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,7 +24,7 @@ int _strlen_recursion(char *s)
 int checker(char *str, int len, int count)
 {
 	if (count >= len)
-	return ((1)
+	return (1);
 	return (checker(str, len - 1, count + 1));
 	return (0);
 }
@@ -36,8 +36,8 @@ int checker(char *str, int len, int count)
  */
 int is_palindrome(char *s)
 {
-int len = _strlen_recursion(s);
-int count = 0;
+	int len = _strlen_recursion(s);
+	int count = 0;
 
-return (checker(s, len - 1, count));
+	return (checker(s, len - 1, count));
 }
