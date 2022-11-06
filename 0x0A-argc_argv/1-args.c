29ff8f5cb37dd80c1d87a81ae6d279b9b1f6e539
Donald Peters <Donny9585@gmail.com>
2022-11-06 23:02:02 +0000

    Done task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the name of this function
+  * @argc: Int argument gotten from command line
+  * @argv: Pointer to char array of strings gotten from command line
+  * Return: 0 upon Success
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
