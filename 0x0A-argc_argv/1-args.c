41809be5cdbf4d9490e62979c8008b44dfa91a51
Donald Peters <Donny9585@gmail.com>
2022-11-06 23:08:18 +0000

    Modified

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the name of this function
-  * @argc: Int argument gotten from command line
-  * @argv: Pointer to char array of strings gotten from command line
-  * Return: 0 upon Success
+  * main - C program that prints the number of srguments passed into it
+  * @argc: Int argument of number of arguments passed to main
+  * @argv: Pointer to char argument of array of strings
+  * Return: O success
   */
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i = 0;
+
+	(void)argv;
+	while (i < argc - 1)
+	{
+		i++;
+	}
+	printf("%d\n", i);
 	return (0);
 }
