34fca575547955e537792e66ab26f1068a69129a
Donald Peters <Donny9585@gmail.com>
2022-11-29 08:45:07 +0000

    Try Again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-  * insert_nodeint_at_index - deletes the head node of a linked list
+  * insert_nodeint_at_index - inserts a new node at a specified index
   *
   * @head: the pointer to the first element of the list
   *
@@ -36,3 +36,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (temp);
 }
+
+
