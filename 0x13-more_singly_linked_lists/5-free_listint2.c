ee11bf5039103d7c9a624ec17d7db33d6ec6bfc3
Donald Peters <Donny9585@gmail.com>
2022-11-29 07:05:57 +0000

    Done task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,15 @@
   *
   * Return: void
   */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	listint_t *temp = head;
+	listint_t *temp = *head;
 
 	while (temp != NULL)
 	{
 		temp = temp->next;
-		free(head);
-		head = temp;
+		free(*head);
+		*head = temp;
 	}
+	*head = NULL;
 }
