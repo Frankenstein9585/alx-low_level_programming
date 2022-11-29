7f313c8ef1fee2860b50ac6bde18b24d6d7bb159
Donald Peters <Donny9585@gmail.com>
2022-11-29 08:37:35 +0000

    Done task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * insert_nodeint_at_index - deletes the head node of a linked list
+  *
+  * @head: the pointer to the first element of the list
+  *
+  * @idx: position to add new node
+  *
+  * @n: data of the new node
+  *
+  * Return: the node at the specified index
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *ptr = *head;
+	unsigned int i = 0;
+
+	temp->n = n;
+	temp->next = NULL;
+
+	for (i = 0; i < idx - 1; ++i)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
+	}
+
+	temp->next = ptr->next;
+	ptr->next = temp;
+
+	if (temp == NULL)
+		return (NULL);
+
+	return (temp);
+}
